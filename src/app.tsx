c4a77a15fdc5c03ab9d11af368581a5ea71be29a
victoria nguyen <vicngan@umich.edu>
2026-02-10 14:49:26 -0500

    continue:

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "@/components/UI/toaster";
+import { Toaster as Sonner } from "@/components/UI/sonner";
+import { TooltipProvider } from "@/components/UI/tool";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./components/layout/MainLayout";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            
+          </Route>
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
